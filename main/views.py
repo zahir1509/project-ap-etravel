2cc38a97ebc25aa5ac39f4ea922b3fdc7b03d6c1
zahir1509 <70563556+zahir1509@users.noreply.github.com>
2020-11-11 19:01:22 +0530

    django auth added

--- main/views.py
+++ main/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
+from django.contrib.auth.forms import UserCreationForm
 
+from .forms import *
 # Create your views here.
 
 def homepage(request):
     return render(request, 'home.html')
+
+def loginPage(request):
+    context = {}
+    return render(request, 'login.html', context)
+
+def signupPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'signup.html', context)
