723a71a65c7325a184d44a2244e7906450101cd4
zahir1509 <70563556+zahir1509@users.noreply.github.com>
2020-11-12 12:15:39 +0530

    auth

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import Http404
 from django.contrib.auth.forms import UserCreationForm
@@ -20,6 +20,7 @@ def signupPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
 
     context = {'form':form}
     return render(request, 'signup.html', context)
