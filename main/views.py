14a7a823cfba4903b855f07702c9f307b7b3572e
zahir1509 <70563556+zahir1509@users.noreply.github.com>
2020-11-14 15:45:38 +0530

    auth complete

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import *
 # Create your views here.
 
@@ -11,38 +12,45 @@ def homepage(request):
     return render(request, 'home.html')
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR Password is incorrect.')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR Password is incorrect.')
             
 
     context = {}
     return render(request, 'login.html', context)
 
+
 def logoutUser(request):
     logout(request)
-    return redirect('login')
+    return redirect('home')
 
 def signupPage(request):
-    form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            name = form.cleaned_data.get('first_name')
-            messages.success(request, 'Your account has been created, ' +  name)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
-            return redirect('login')
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                name = form.cleaned_data.get('first_name')
+                messages.success(request, 'Your account has been created, ' +  name)
+
+                return redirect('login')
 
     context = {'form':form}
     return render(request, 'signup.html', context)
