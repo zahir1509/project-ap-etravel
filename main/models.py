fefc22871837123fe7a0fef4bc7248a44aa98ae5
zahir1509 <70563556+zahir1509@users.noreply.github.com>
2020-11-27 16:56:13 +0530

    update

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
+from datetime import datetime, date
 # Create your models here.
 
 class Location(models.Model):
